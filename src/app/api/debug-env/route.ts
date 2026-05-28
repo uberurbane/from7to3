dd8147dddb1833a0a8ba002472806cea88bfc4f0
uberurbane <uberurbane@gmail.com>
2026-05-28 13:51:21 -0500

    Remove temporary debug API route

--- src/app/api/debug-env/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    hasUrl: Boolean(process.env.NEXT_PUBLIC_SUPABASE_URL),
-    hasAnon: Boolean(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY),
-    hasServiceRole: Boolean(process.env.SUPABASE_SERVICE_ROLE_KEY),
-  });
-}
